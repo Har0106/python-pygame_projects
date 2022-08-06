294b5e83c2b5a3e2259a77ce74b72eafa05cc3c5
Har0106 <freddiemaron@gmail.com>
2022-08-06 20:41:41 +0530

    moving the player car

--- car_game.py
+++ car_game.py
@@ -7,7 +7,12 @@ road_width = width//1.3
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Car Game')
 
-def show():
+# car of the user
+user_car_image = pygame.transform.scale(pygame.image.load('car_images/car.png'), (150, 150))
+# car of the user
+game_car_image = pygame.transform.scale(pygame.image.load('car_images/otherCar.png'), (150, 150))
+
+def show(user_car, game_car):
     # giving a background color
     window.fill((50, 205, 50))
 
@@ -19,28 +24,34 @@ def show():
     pygame.draw.rect(window, (255, 255, 255), (width//2 - road_width//2 + 16, 0, 8, height))
     pygame.draw.rect(window, (255, 255, 255), (width//2 + road_width//2 - 22, 0, 8, height))
 
-    # car of the user
-    user_car = pygame.transform.scale(pygame.image.load('car_images/car.png'), (150, 150))
-    window.blit(user_car, (road_width - 120, height - user_car.get_width() - 10))
-
-    # car of the user
-    game_car = pygame.transform.scale(pygame.image.load('car_images/otherCar.png'), (150, 150))
-    window.blit(game_car, (road_width - 295, 10))
+    # displying the cars
+    window.blit(user_car_image, (user_car.x, user_car.y))
+    window.blit(game_car_image, (game_car.x, game_car.y))
 
     pygame.display.update()
 
 # mainloop
 def main():
+    user_car = pygame.Rect(road_width - 120, height - 160, 150, 150)
+    game_car = pygame.Rect(road_width - 295, 10, 150, 150)
+
     run = True
     while run:
         for event in pygame.event.get():
+            # quitting the game
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 return
+            # moving according to the keys pressed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and user_car.x - 175 > 0:
+                    user_car.x -= 175
+                if event.key == pygame.K_RIGHT and user_car.x + 175*2 < width:
+                    user_car.x += 175
 
         # show things onto the screen
-        show()
+        show(user_car, game_car)
 
 main()
         
